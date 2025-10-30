546d5b743385407047cc2dcd15ae0066a911d385
Evangelista96 <joaoambrosio78@gmail.com>
2025-10-30 13:35:30 -0300

    Card e Lista

---
+++ src/components/tema/listatemas/ListaTemas.tsx
@@ -0,0 +1,83 @@
+
+
+import { buscar } from "../../../services/Service";
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import type { Tema } from "../../../models/Tema";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { SyncLoader } from "react-spinners";
+import Cardtema from "../cardtema/CardTema";
+
+
+function ListaTemas() {
+    const navigate = useNavigate();
+
+    const[isLoading, setIsLoading] = useState<boolean>(false)
+
+    const [temas, setTemas] = useState<Tema[]>([])
+
+    const {usuario, handleLogout} = useContext(AuthContext)
+    const token = usuario.token
+
+    useEffect(() => {
+        if (token === "") {
+            alert("Você precisa estar logado")
+            navigate("/")
+        }
+        }, [token])
+
+    useEffect(() => {
+        buscarTemas()
+    }, [temas.length])
+
+    async function buscarTemas() {
+    try {
+        setIsLoading(true)
+     await buscar("/temas", setTemas, {
+        headers: {
+            Authorization: token
+        }
+     })
+    } catch (error:any) {
+        if (error.toString().includes("401")) {
+            handleLogout()
+        }
+    } finally {
+        setIsLoading(false)
+    }
+    }
+
+    return (
+        <>
+     {isLoading && (
+    <SyncLoader
+        color="#312e81"
+    	size={32}
+	/>
+)}
+
+        <div className='flex justify-center w-full my-4'>
+            <div className='container flex flex-col'>
+                {(!isLoading && temas.length === 0) && (
+	<span className="text-3xl text-center my-8">
+		Nenhum Tema foi encontrado!
+	</span>
+)}
+                <div className='grid grid-cols-1 md:grid-cols-2 
+                                lg:grid-cols-3 gap-8'>
+                        {
+    temas.map((tema) => (
+    	<Cardtema key={tema.id} tema={tema}/>
+    ))
+}
+                        
+                </div>
+            </div>
+        </div>
+        
+        </>
+    )
+}
+
+
+export default ListaTemas;
